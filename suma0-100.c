1512a3f84f06de9200eef7166989cb72d3199d89
tonycuantico <159995704+tonycuantico@users.noreply.github.com>
2024-08-30 14:49:28 -0600

    Create suma0-100.c

---
+++ suma0-100.c
@@ -0,0 +1,19 @@
+/* Rodriguez Arreguin Luis Antonio*/
+#include <stdio.h>
+
+int i;
+int suma =0;
+
+int main ()
+{
+  for (i = 0;  i <= 100; i++)
+   {
+     suma = suma + i;
+     }
+
+     printf("La suma de los numeros del 0 al 100 es: %d\n", suma);
+       return 0;
+  }
+  
+     
+   
